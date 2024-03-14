d99c88ff00c68c2db867eb8a3d07bac7da8506f1
lovissaprice-pvcc <159392346+lovissaprice-pvcc@users.noreply.github.com>
2024-03-13 20:53:56 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#Name: Lovissa Price
+#Prog Purpose: Hello World
+
+print("Hello world! This is my pirst Python program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots of other things!")
